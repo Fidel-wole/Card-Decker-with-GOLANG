e0edf03cf4726331c8a81c1d55be60e8e6a6b474
Adewole Adewoye <adewoyeadedayo10@gmail.com>
2024-02-09 08:09:22 +0100

    feat: add deck func

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+
+
+func main() {
+
+cards := newDeckFromFile("my_cards")
+cards.print()
+	// hand, remainingCards := deal(cards, 5)
+	// hand.print()
+	// remainingCards.print()
+
+
+}
